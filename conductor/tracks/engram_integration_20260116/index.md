aaf866e082d59b20ec72c1e8bb898ddee13c8ce9
Todd <pwnzersaurus>
2026-01-19 16:09:16 -0600

    chore: cleanup repository for public release

--- conductor/tracks/engram_integration_20260116/index.md
+++
@@ -1,5 +0,0 @@
-# Track engram_integration_20260116 Context
-
-- [Specification](./spec.md)
-- [Implementation Plan](./plan.md)
-- [Metadata](./metadata.json)
--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /data/data/com.termux/files/home/project/SENSE/SENSE
-export PYTHONPATH=/data/data/com.termux/files/home/project/SENSE/SENSE:$PYTHONPATH
-python run_tests.py
